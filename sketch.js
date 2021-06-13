30a2a2fa12476f73c603541c8f8debc8cc3b3f2a
Matt Russell <russell.d.matt@gmail.com>
2021-06-13 15:03:01 -0700

    added lines

--- sketch.js
+++ sketch.js
@@ -6,17 +6,26 @@
 // Then, on each frame, we take one we take one point off the
 // [to_reflect_horizontal] and [to_reflect_radian] arrays and reflect
 // them across the horizontal and 1 radian line, respectively.
-
 to_reflect_horizontal = [];
 to_reflect_radian = [];
 
 function setup() {
   createCanvas(640, 640);
   angleMode(RADIANS);
-  strokeWeight(5);
+  strokeWeight(8);
   translate(320, 320);
+  scale(1, -1);
   stroke("green");
   point(0, 0);
+  // draw lines of symmetry
+  strokeWeight(2);
+  stroke("gray");
+  dx = width / 2;
+  line(-dx, 0, dx, 0);
+  h = tan(1) * dx;
+  line(-dx, -h, dx, h);
+  // set stroke color for reflected points
+  strokeWeight(5);
   stroke("black");
 }
 
@@ -38,14 +47,15 @@ function rotate_radian([x, y]) {
 
 function draw() {
   translate(width / 2, height / 2);
-  if (to_reflect_radian.length > 0) {
+  scale(1, -1);
+  if (to_reflect_horizontal.length > 0) {
     [x, y] = rotate_horizontal(to_reflect_horizontal.shift());
     point(x, y);
     to_reflect_radian.push([x, y]);
   }
-  //
   if (to_reflect_radian.length > 0) {
-    [x, y] = rotate_radian(to_reflect_radian.shift());
+    [x, y] = to_reflect_radian.shift();
+    [x, y] = rotate_radian([x, y]);
     point(x, y);
     to_reflect_horizontal.push([x, y]);
   }
@@ -53,10 +63,10 @@ function draw() {
 
 function mouseClicked() {
   x = mouseX - width / 2;
-  y = mouseY - height / 2;
+  y = -(mouseY - height / 2);
   point(x, y);
   let p = [x, y];
-  console.log(p);
+
   to_reflect_horizontal.push(p);
   to_reflect_radian.push(p);
 }
